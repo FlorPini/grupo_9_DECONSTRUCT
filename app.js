1abf99df7b4fa3e9570ecc4a65e77590e5d8aa50
Joaquin Failo <73504599+Joafai@users.noreply.github.com>
2022-02-08 15:01:08 -0300

    login cambios

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-const express = require ("express");
-const path = require ("path");
-const app = express ();
+const express = require("express");
+const path = require("path");
+const app = express();
 
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.listen (3000, () => {
-    console.log("servidor corriendo");
-})
+app.listen(3000, () => {
+  console.log("servidor corriendo");
+});
 
-app.get ("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
 
-app.get ("/product", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/product.html"));
-})
+app.get("/product", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/product.html"));
+});
 
-app.get ("/shopping-cart", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views//shopping-cart.html"));
-})
+app.get("/shopping-cart", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views//shopping-cart.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views//login.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views//register.html"));
+});
