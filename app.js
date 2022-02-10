984849e395ced5cf655269d6c8979410cd315d80
Joaquin Failo <73504599+Joafai@users.noreply.github.com>
2022-02-10 13:11:52 -0300

    login cambios 2

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
 const express = require("express");
 const path = require("path");
 const app = express();
+const rutasProductos = require("./routes/productos.js");
+const rutasMain = require("./routes/main.js");
+const rutasShopping = require("./routes/shopping.js");
 
-=======
-const express = require ("express");
-const path = require ("path");
-const app = express ();
-const rutasProductos = require ("./routes/productos.js");
-const rutasMain = require ('./routes/main.js');
-const rutasShopping = require ('./routes/shopping.js');
->>>>>>> 4abd77eb586df7e00049941399bb878360571261
 const publicPath = path.resolve(__dirname, "./public");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.static(publicPath));
 
@@ -21,31 +15,8 @@ app.listen(3000, () => {
   console.log("servidor corriendo");
 });
 
-<<<<<<< HEAD
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/home.html"));
-});
+app.use("/", rutasMain);
 
-app.get("/product", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/product.html"));
-});
+app.use("/productos", rutasProductos);
 
-app.get("/shopping-cart", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views//shopping-cart.html"));
-});
-
-app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views//login.html"));
-});
-
-app.get("/register", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views//register.html"));
-});
-=======
-app.use('/', rutasMain);
-
-app.use('/productos', rutasProductos);
-
-app.use('/shopping-cart', rutasShopping);
-
->>>>>>> 4abd77eb586df7e00049941399bb878360571261
+app.use("/shopping-cart", rutasShopping);
--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,11 @@
-const express = require ("express");
-const router = express.Router ();
-let mainController = require ('../Controllers/mainController')
+const express = require("express");
+const router = express.Router();
+let mainController = require("../Controllers/mainController");
 
+router.get("/", mainController.index);
 
-router.get('/', mainController.index )
+router.get("/register", mainController.register);
 
-router.get('/register', mainController.register)
-
-router.get('/login', mainController.login)
+router.get("/login", mainController.login);
 
 module.exports = router;
