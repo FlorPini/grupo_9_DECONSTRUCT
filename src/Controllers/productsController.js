167ec9d90a65bd6f9b608dab87f66f7c7244077c
Malterio <m_alterio@yahoo.com.ar>
2022-04-24 00:44:52 -0300

    Products DB

--- src/Controllers/productsController.js
+++ src/Controllers/productsController.js
@@ -7,14 +7,25 @@ const jsonTable= require("../database2/jsonTable");
 const productsModel = jsonTable ("products")
 const db = require("../database/models");
 
+
 let productsController = {
     index: (req, res) => {
         let products = productsModel.all() 
         return res.render('./products/productsList' , {products});
     },
+    
+    
     create: (req, res) => {
-        return res.render('./products/create');
-    },
+        
+        Promise.all([db.Category.findAll(),db.Type.findAll()])        
+            .then(function([categorys, types]){     
+            return res.render('./products/create',{types: types , categorys: categorys})
+            })
+            .catch(function(error){
+                console.log(error);
+            })
+        },
+
     store: (req, res) => {
         let errors = validationResult(req);
         if ( errors.isEmpty()){
@@ -23,7 +34,12 @@ let productsController = {
                 productId = productsModel.create(product);
                 res.redirect('/products/' + productId);
                 } else {
-                res.render('./products/create', { errors: errors.mapped(), old: req.body}); //le enviamos a la vista de creacion de producto un array con los errores
+                    Promise.all([db.Category.findAll(),db.Type.findAll()])        
+                    .then(function([categorys, types]){ 
+                        console.log(req.body);    
+                        res.render('./products/create', {errors: errors.mapped(), old: req.body, types: types , categorys: categorys});
+                    })
+                 //le enviamos a la vista de creacion de producto un array con los errores
                 }
       //  let newItem = {
        //     name : req.body.productName ,
